c5fb21476f9878f856d2aa067b51c6e767e6946c
Shauli Ravfogel <shauli321@gmail.com>
2020-04-13 15:22:36 +0300

    supportng different kidns of queries

--- spike_queries.py
+++ spike_queries.py
@@ -16,29 +16,24 @@ def get_tsv_url(response: requests.models.Response, results_limit: int, base_url
     tsv_url = base_url + tsv_location +"?sentence_text=True&capture_indices=True&sentence_id=True&limit={}".format(results_limit)
     return tsv_url
     
-@st.cache    
-def perform_query(query: str, dataset_name: str = "pubmed", num_results: int = 10) -> pd.DataFrame:
+@st.cache(allow_output_mutation=True)    
+def perform_query(query: str, dataset_name: str = "pubmed", num_results: int = 10, query_type : str = "syntactic") -> pd.DataFrame:
 
         
    template = """{{
-  "queries": {{"syntactic": "{query_content}"}},
+  "queries": {{"{query_type}": "{query_content}"}},
   "data_set_name": "{dataset_name}",
   "include_annotations": true
 }}"""
-   
-   print(template)        
         
-   query = template.format(query_content = query, dataset_name = dataset_name)
+   query = template.format(query_content = query, dataset_name = dataset_name, query_type = query_type)
    headers = {'content-type': 'application/json'}
    if dataset_name == "pubmed":
         url, base_url = PUBMED_URL, PUBMED_BASE_URL
    elif dataset_name == "covid19":
         url, base_url = COVID_URL, COVID_BASE_URL
    
-   print(query)
-   
    response = requests.post(url, data = query, headers = headers)
-   print(response)
    
    tsv_url = get_tsv_url(response, results_limit = num_results, base_url = base_url)
    df = pd.read_csv(tsv_url, sep = "\t")
