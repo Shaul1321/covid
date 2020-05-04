c95eb97e09f5472b3e601bc101f8caa1fe8ccb78
Shauli Ravfogel <shauli321@gmail.com>
2020-05-04 14:00:03 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-git clone https://github.com/Shaul1321/covid.git
+#git clone https://github.com/Shaul1321/covid.git
 python3 run_bert.py ----input-filenam results.tsv --device cpu --pooling cls --output_fname output-cls.jsonl
 python3 build_index.py --fname output-cls.jsonl --num_vecs_pca 100000 --pca_variance 0.985 --similarity_type cosine
