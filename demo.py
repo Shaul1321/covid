ff0ee1759c6ab0289bcad6f09c5ea032a1b50d58
Shauli Ravfogel <shauli321@gmail.com>
2020-05-24 17:46:07 +0300

    Update demo.py

--- demo.py
+++ demo.py
@@ -13,7 +13,7 @@ import time
 @st.cache(allow_output_mutation=True)
 def load_sents_and_ids():
     with st.spinner('Loading sentences and IDs...'):
-        df = pd.read_csv("covid-all-sents-2.4.csv", sep = "\t")
+        df = pd.read_csv("results.tsv", sep = "\t")
         sents =  df["sentence_text"].tolist()
         ids = [hash(s) for s in sents]
 
@@ -25,7 +25,7 @@ def load_sents_and_ids():
 @st.cache(allow_output_mutation=True)
 def load_index(similarity, pooling):
     with st.spinner('Loading FAISS index...'):
-        fname = "output.new." + pooling + ".index"
+        fname = "output." + pooling + ".index"
         index = faiss.read_index(fname)
         return index
 
@@ -38,7 +38,7 @@ def load_bert():
 @st.cache(allow_output_mutation=True)        
 def load_pca(pooling):
 
-    fname = "output.new." + pooling + ".pca.pickle"
+    fname = "output." + pooling + ".pca.pickle"
     with open(fname, "rb") as f:
     
         return pickle.load(f)
